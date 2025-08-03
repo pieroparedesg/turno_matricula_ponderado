58846481c43dfb0c9f5417b126ce80378e39d5ef
Uri-Alfo <pparedesgalvez@gmail.com>
2025-08-03 18:44:41 -0500

    fix: error de sintaxis en layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-dimport type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
